b885ddafc165fd1e00ee61175740efe5149b4123
nothub <48992448+nothub@users.noreply.github.com>
2021-12-01 12:47:44 +0100

    day 1 streams

--- src/main/java/not/hub/aoc2021/day1/SonarSweep1.java
+++ src/main/java/not/hub/aoc2021/day1/SonarSweep1.java
@@ -3,20 +3,17 @@ package not.hub.aoc2021.day1;
 import not.hub.aoc2021.Solver;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class SonarSweep1 extends Solver<List<Integer>, Integer> {
 
     @Override
     public Integer solve(List<Integer> input) {
-
-        int increases = 0;
-        for (int i = 0; i < input.size(); i++) {
-            if (i == input.size() - 1) break;
-            if (input.get(i) < input.get(i + 1)) increases++;
-        }
-
-        return increases;
-
+        return (int) IntStream
+            .range(0, input.size())
+            .takeWhile(i -> i < input.size() - 1)
+            .filter(i -> input.get(i) < input.get(i + 1))
+            .count();
     }
 
 }
--- src/main/java/not/hub/aoc2021/day1/SonarSweep2.java
+++ src/main/java/not/hub/aoc2021/day1/SonarSweep2.java
@@ -2,23 +2,21 @@ package not.hub.aoc2021.day1;
 
 import not.hub.aoc2021.Solver;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class SonarSweep2 extends Solver<List<Integer>, Integer> {
 
     @Override
     public Integer solve(List<Integer> input) {
-
-        List<Integer> windows = new ArrayList<>();
-
-        for (int i = 0; i < input.size(); i++) {
-            if (i == input.size() - 2) break;
-            windows.add(input.get(i) + input.get(i + 1) + input.get(i + 2));
-        }
-
-        return new SonarSweep1().solve(windows);
-
+        return new SonarSweep1().solve(
+            IntStream
+                .range(0, input.size())
+                .takeWhile(i -> i < input.size() - 2)
+                .mapToObj(i -> input.get(i) + input.get(i + 1) + input.get(i + 2))
+                .collect(Collectors.toList())
+        );
     }
 
 }
