e999bc46a556c3bd80daed35aa0cdb1108f05424
nothub <48992448+nothub@users.noreply.github.com>
2021-12-03 13:34:00 +0100

    day 3 part 1 cleanup

--- src/main/java/not/hub/aoc2021/day3/BinaryDiagnostic1.java
+++ src/main/java/not/hub/aoc2021/day3/BinaryDiagnostic1.java
@@ -8,28 +8,38 @@ import java.util.stream.Collectors;
 
 public class BinaryDiagnostic1 implements Solver<List<String>, Integer> {
 
-    @Override
-    public Integer solve(List<String> input) {
-        int wordLength = input.get(0).length();
-        int[] rotated = new int[wordLength];
-        for (int i = 0; i < wordLength; i++) {
+    static int[] rotate(List<String> input) {
+        int[] rotated = new int[input.get(0).length()];
+        for (int i = 0; i < input.get(0).length(); i++) {
             for (String s : input) {
                 rotated[i] += Integer.parseInt(s.substring(i, i + 1));
             }
         }
-        int[] common = new int[wordLength];
-        for (int i = 0; i < rotated.length; i++) {
-            if (rotated[i] > input.size() / 2) common[i] = 1;
-            else common[i] = 0;
-        }
-        int gamma = Integer.parseInt(Arrays.stream(common).mapToObj(i -> "" + i).collect(Collectors.joining()), 2);
-        int[] inverted = new int[wordLength];
+        return rotated;
+    }
+
+    static int[] invert(int[] common) {
+        int[] inverted = new int[common.length];
         for (int i = 0; i < common.length; i++) {
             if (common[i] == 1) inverted[i] = 0;
             else inverted[i] = 1;
         }
-        int epsilon = Integer.parseInt(Arrays.stream(inverted).mapToObj(i -> "" + i).collect(Collectors.joining()), 2);
-        return gamma * epsilon;
+        return inverted;
+    }
+
+    static int parse(int[] arr) {
+        return Integer.parseInt(Arrays.stream(arr).mapToObj(String::valueOf).collect(Collectors.joining()), 2);
+    }
+
+    @Override
+    public Integer solve(List<String> input) {
+        int[] rotated = rotate(input);
+        int[] common = new int[input.get(0).length()];
+        for (int i = 0; i < rotated.length; i++) {
+            if (rotated[i] > input.size() / 2) common[i] = 1;
+            else common[i] = 0;
+        }
+        return parse(common) * parse(invert(common));
     }
 
 }
